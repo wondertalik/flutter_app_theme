63a7b230ad5b14828bac60d18e230e058a6f5aa4
Vitalii Koretskyi <wondertalik@gmail.com>
2021-09-28 18:58:54 +0200

    refactor: use listview instead of singlechildscrollview

--- lib/demo/view/demo_content.dart
+++ lib/demo/view/demo_content.dart
@@ -6,32 +6,29 @@ class DemoContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 8, right: 8, top: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            ColorSchemeWidget(),
-            ComponentSpacer(),
-            Buttons(),
-            ComponentSpacer(),
-            TopAppBar(),
-            ComponentSpacer(),
-            ComponentSpacer(),
-            Options(),
-            ComponentSpacer(),
-            Progress(),
-            ComponentSpacer(),
-            Chips(),
-            ComponentSpacer(),
-            Fabs(),
-            ComponentSpacer(),
-            Texts(),
-            ComponentSpacer(),
-            TextFields(),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.only(left: 8, right: 8, top: 16),
+      child: ListView(
+        children: const [
+          ColorSchemeWidget(),
+          ComponentSpacer(),
+          Buttons(),
+          ComponentSpacer(),
+          TopAppBar(),
+          ComponentSpacer(),
+          ComponentSpacer(),
+          Options(),
+          ComponentSpacer(),
+          Progress(),
+          ComponentSpacer(),
+          Chips(),
+          ComponentSpacer(),
+          Fabs(),
+          ComponentSpacer(),
+          Texts(),
+          ComponentSpacer(),
+          TextFields(),
+        ],
       ),
     );
   }
